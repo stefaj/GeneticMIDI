6a871f4a06b8fec05396ce0c740f9ae38f1ebace
1gn1t0r <stefanjacholke@gmail.com>
2015-10-16 16:48:02 +0200

    Dummy class for addition of existing tracks

---
+++ GeneticMIDI/Generators/Sequence/ExistingSequenceGenerator.cs
@@ -0,0 +1,40 @@
+﻿using GeneticMIDI.Representation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticMIDI.Generators.Sequence
+{
+    public class ExistingSequenceGenerator : INoteGenerator
+    {
+        MelodySequence sequence;
+        PatchNames instrument;
+        public ExistingSequenceGenerator(MelodySequence sequence, PatchNames instrument)
+        {
+            this.sequence = sequence;
+            this.instrument = instrument;
+        }
+
+        public MelodySequence Generate()
+        {
+            return sequence;
+        }
+
+        public MelodySequence Next()
+        {
+            return Generate();
+        }
+
+        public bool HasNext
+        {
+            get { return true; }
+        }
+
+        public PatchNames Instrument
+        {
+            get { return instrument; }
+        }
+    }
+}
