8ef9887318f3658ccd3394d54d1e2deedc6a52b5
1gn1t0r <stefanjacholke@gmail.com>
2015-10-16 11:47:43 +0200

    some more constants

--- GeneticMIDI/Constants.cs
+++ GeneticMIDI/Constants.cs
@@ -9,5 +9,9 @@ namespace GeneticMIDI
     public static class Constants
     {
         public static string LIBRARY_PATH = "";
+
+        public static string LOCAL_LIBRARY_PATH = "lib";
+
+        public static string LOCAL_SAVE_PATH = "save";
     }
 }
